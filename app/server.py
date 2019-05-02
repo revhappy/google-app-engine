85cddaf2aebebbb52c007015e224d55b41693bbe
Robb Sharma <robbsharma@gmail.com>
2019-05-02 02:13:14 -0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -54,7 +54,7 @@ async def analyze(request):
     img_bytes = await (data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = str(learn.predict(img)[0])
-    return JSONResponse({'result': str(prediction)})
+    return JSONResponse({'result':str(learn.predict(img)[0])}
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=8080)
